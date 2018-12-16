21ff447b849f5cb101b2a30a03ac854155010138
Mohan AMILINENI <amilineni4u@gmail.com>
2018-12-16 11:47:17 +1000

    tomee changes

--- src/main/java/com/ag/todoservices/TodoTaskServicesApplication.java
+++ src/main/java/com/ag/todoservices/TodoTaskServicesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoTaskServicesApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("tomcat.util.http.parser.HttpParser.requestTargetAllow", "[](){}");
 		SpringApplication.run(TodoTaskServicesApplication.class, args);
 	}
 
