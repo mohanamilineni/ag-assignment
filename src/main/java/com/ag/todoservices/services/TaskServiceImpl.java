4181caa0ebe5eaf2d663a0ff8838b77eda91d42d
Mohan AMILINENI <amilineni4u@gmail.com>
2018-12-17 06:44:09 +1000

    brackets changes

--- src/main/java/com/ag/todoservices/services/TaskServiceImpl.java
+++ src/main/java/com/ag/todoservices/services/TaskServiceImpl.java
@@ -4,10 +4,11 @@
  */
 package com.ag.todoservices.services;
 
-import java.util.Stack;
-
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.stereotype.Service;
-
 import com.ag.todoservices.domain.Task;
 
 
@@ -35,12 +36,15 @@ public class TaskServiceImpl implements TaskService {
 	 */
 	@Override
 	public Task validateBrackets(Task task) {
-		task.setIsBalanced(isValid(task.getInput().trim()));	
+		task.setIsBalanced(isBalanced(task.getInput().trim()));	
 		return task;
 	}
 
 	/******************* PRIVATE APIs*************************/
 	
+	
+	final Map<Character, Character> brackets = new HashMap<Character, Character>() {{put('{', '}');put('(', ')');put('[', ']');}};    
+
 	/**
 	 * Evaluates input string and determines the string is balanced or not.
 	 * 	 * Criteria is to have every opening bracket must have its corresponding bracket in an ordered form.
@@ -52,61 +56,34 @@ public class TaskServiceImpl implements TaskService {
 	 * @return boolean true if the string is balanced.
 	 *
 	 */
-	private boolean isValid(String bracerStr) {
-		
+    public boolean isBalanced(String strToCheck) {
 		//0- Declarations
-		boolean isBalanced 			    = false;
-		char[] bracerArr                = bracerStr.toCharArray();
-		Stack<Character> openBraceStack = new Stack<Character>();
-		
-		char braceChar;
+        Deque<Character> openBraceStack = new ArrayDeque<Character>();        
+        int strLength = strToCheck.length();
+        
 		//1- Iterate through list of braces given
-		for (int i=0; i < bracerArr.length ; i++) {
-			
-			braceChar = bracerArr[i];
-			
+        for (int i = 0; i < strLength; i++) {
+            char currentChar = strToCheck.charAt(i);
+            
 			//1.1 - if it is opening type brace, then put it inside stack
-			if(braceChar == '{' || braceChar == '[' || braceChar == '(') {
-				openBraceStack.push(braceChar);
-			}
-			
-			//1.2 - if it is closing type brace, then go inside
-			else if(braceChar == '}' || braceChar == ']' || braceChar == ')') {
-				
-				//1.2.1 - if first character itself closing brace, then break and also if not balanced return false.
-				if(openBraceStack.isEmpty() || !isBalanced(openBraceStack.pop(), braceChar)) {
-					break;
-				}
-				//1.2.2 - Take first element of stack and compare it with closing brace, if it matches balanced true for given pair and move ahead for next char.
-				else {
-					isBalanced = true;
-					continue;
-				}
-			}
-		}
-		//1.3 after all the matches, if the characters still left over in the stack then the string is not balanced 
-		if(!openBraceStack.isEmpty()) isBalanced = false;
-		
-		return isBalanced;
-	}
-	
-	/**Compares opening and closing char and returns true if it is balanced.
-	 * 	
-	 * @param openingChar
-	 * @param closingChar
-	 * @return isBalanced true if the opening and closing char forms balance.
-	 */
-	private boolean isBalanced(char openingChar, char closingChar) {		
-		//0 - check for parentheses
-      	if (openingChar == '(' && closingChar == ')') 		return true;
-      	
-		//1 - check for flower braces
-        else if (openingChar == '{' && closingChar == '}')	return true;
-      	
-		//2 - check for square braces
-        else if (openingChar == '[' && closingChar == ']')  return true;
-      	
-		//3 - return false if above condition fails.
-        else return false;		
-	}
+            if (isOpenBracket(currentChar)) openBraceStack.push(currentChar);
+            
+			//1.2 - if it is closing type brace, then check is this matching with corresponding closing brace of LIFO element 
+            else if (isClosingBracket(currentChar) && isNotMatching(openBraceStack, currentChar)) return false;            
+        }
+        //2 - if any of the braces still left over in stack, then string is not balanced.
+        return openBraceStack.isEmpty();
+    }
+    
+    private boolean isNotMatching(Deque<Character> openBraceStack, char currentChar) {
+        return openBraceStack.size() == 0 || !brackets.get(openBraceStack.pop()).equals(currentChar);
+    }
+
+    private boolean isClosingBracket(char currentChar) {
+        return brackets.values().contains(currentChar);
+    }
+
+    private boolean isOpenBracket(char currentChar) {
+        return brackets.containsKey(currentChar);
+    }
 }
